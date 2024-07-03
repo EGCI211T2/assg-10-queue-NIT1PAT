5d49e0896b024ef3b7be07335aba9f05b07ba9f2
NIT1PAT <167950972+NIT1PAT@users.noreply.github.com>
2024-07-03 04:45:07 +0000

    done

--- Queue.h
+++ Queue.h
@@ -2,6 +2,7 @@
 #ifndef queue_h
 #define queue_h
 #include "Node.h"
+#include <cstring>
 class Queue {
 	NodePtr headPtr,tailPtr;
 	int size;
@@ -16,20 +17,32 @@ public:
 void Queue::enqueue(int x){
   NodePtr new_node= new NODE(x);
 if(new_node){ 
-    /* Add head and tail for me please */
-   1. connect & Change tail
-  2. (may be) change head  when the queue is empty
-  3. increase size*/
+  //Add head and tail for me please 
+  //1. connect & Change tail
+  if(headPtr != NULL)
+  {
+    tailPtr->set_next(new_node);
+    tailPtr = new_node;
+  }
+  //2. (may be) change head  when the queue is empty
+  else {
+    headPtr = new_node;
+    tailPtr = new_node;
+  }
+  //3. increase size 
+  size++;
 	
  }
 }
 
 int Queue::dequeue(){
   if(headPtr!=NULL){
-     NodePtr t=headPtr;
-     int value= t->get_value();
+     NodePtr t = headPtr;
+     int value = t->get_value();
      /* Add head and tail for me please */
-          
+      headPtr = headPtr->get_next();
+      size--;
+
      delete t;
      return value;
   }
@@ -40,10 +53,23 @@ int Queue::dequeue(){
 
 Queue::Queue(){
     //initialize Queue
+    size = 0;
+    headPtr = NULL;
+    tailPtr = NULL;
     
 }
 Queue::~Queue(){
     //delete all remaning Queue (i.e. DQ all) 
+    cout<<"Clearing Queue"<<endl;
+
+    for(int i=0; i<size; i++)
+    {
+      NodePtr t = headPtr;
+      int value = t->get_value();
+      headPtr = headPtr->get_next();
+      cout<<"Dequeing "<< t->get_value()<<endl;
+      delete t;
+    }
     
 }
 
